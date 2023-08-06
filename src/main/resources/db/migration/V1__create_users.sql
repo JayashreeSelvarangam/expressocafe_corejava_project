2ca5e790bde45d84b0c9e6eee0e738c44f50e364
JayashreeSelvarangam <116268739+JayashreeSelvarangam@users.noreply.github.com>
2023-08-06 23:14:12 +0530

    Update V1__create_users.sql

--- src/main/resources/db/migration/V1__create_users.sql
+++ src/main/resources/db/migration/V1__create_users.sql
@@ -1,41 +1,42 @@
-create database newone1;
-use newone1;
-CREATE TABLE IF NOT EXISTS products(
-product_id INT PRIMARY KEY NOT NULL,
-productname VARCHAR(50)NOT NULL,
-category_id INT NOT NULL,
-FOREIGN KEY (category_id) REFERENCES category(category_id) 
+create database express_cafe;
+use express_cafe;
+CREATE TABLE users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    phone_number BIGINT NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    age INT NOT NULL
+);
+CREATE TABLE category (
+    cate_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE size (
+    size_id INT PRIMARY KEY AUTO_INCREMENT,
+    size_name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS sizes (
-size_id INT PRIMARY KEY NOT NULL,
-size_name VARCHAR(50) UNIQUE
+CREATE TABLE product (
+    product_id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    status BOOLEAN DEFAULT TRUE,
+    category_id INT,
+    created_date DATETIME NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES category (cate_id)
 );
 
-CREATE TABLE IF NOT EXISTS category (
-category_id INT PRIMARY KEY NOT NULL,
-createdDate DATE NOT NULL,
-endDate DATE
+CREATE TABLE price (
+    price_id INT PRIMARY KEY AUTO_INCREMENT,
+    product_id INT NOT NULL,
+    size_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME,
+    FOREIGN KEY (product_id) REFERENCES product (product_id),
+    FOREIGN KEY (size_id) REFERENCES size (size_id)
 );
-
-
-CREATE TABLE IF NOT EXISTS prices (
-price_id INT PRIMARY KEY NOT NULL,
-category_id INT NOT NULL,
-size_id INT NOT NULL ,
-product_id INT NOT NULL,
-FOREIGN KEY (category_id) REFERENCES category (category_id),
-FOREIGN KEY (product_id) REFERENCES products(product_id)
-);
- 
-ALTER TABLE prices
-ADD COLUMN createdDate DATE NOT NULL;
-ALTER TABLE prices
-ADD COLUMN endDate DATE;
-
-
-ALTER TABLE prices
-ADD CONSTRAINT fk_constraint_name -- Provide a name for the foreign key constraint (optional, but recommended)
-FOREIGN KEY (size_id)    -- The column in table1 that will reference the foreign key
-REFERENCES sizes(size_id); -- The referenced table and column
 
